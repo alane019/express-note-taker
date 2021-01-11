f974dcbf0dd07d1c65438de73b6a2ff2648ce322
Adam <alane019@gmail.com>
2021-01-11 01:12:30 -0500

    add app.js with static routing in public directory

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// dependencies
+const express = require("express");
+
+ const app = express();
+ // set port with NodeJS.Process.env: 
+ let PORT = process.env.PORT || 8080;
+
+//middleware: handle json and data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// serve static files in designated public directory
+// variant >> app.use(express.static("public"));
+app.use(express.static("./public", {
+	extensions: ['html', 'js', 'css'],
+}));
+
+// at server start, express.listen takes a port and callback err handler
+app.listen(PORT, function(err) {
+	console.log("Listening on PORT: " + PORT);      
+});
+  
