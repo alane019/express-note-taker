09d26ef7da0cbdbd82a0de8e6e3e39aec7eecee5
Adam <alane019@gmail.com>
2021-01-11 12:49:33 -0500

    updated apiRoutes with post method to save new notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,67 @@
-//const { json } = require("express");
-const { ifError } = require("assert");
 const fs = require("fs");
 const path = require("path"); 
 const dbDir = path.resolve(__dirname, "../db");
+let notes = [];
 
+/*
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+*/
 
 module.exports = function(app){ 
-
+	let notes = [];
 // serve url routes for client-side js file
 // following convention: api in url path for json files
-	// get method will be shortest
-  app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function(req, res) {
 
- let notes = fs.readFileSync(path.resolve(dbDir, "db.json"), "utf8");
- // might need to parse json
-     //.... notes = Array.from(notes);
-	 //..... return res.json(notes);
-	 //......return res.notes;
- notes = JSON.parse(notes);
- return res.json(notes); //res.json(notes);
-	
-  });
+	notes = fs.readFileSync(path.resolve(dbDir, "db.json"), "utf8");
+	notes = JSON.parse(notes);
+	return res.json(notes); 
+});
+
 
 // save notes
+app.post("/api/notes", function(req, res) {
+	let notes = fs.readFileSync(path.resolve(dbDir, "db.json"), "utf8");
+	notes = JSON.parse(notes);
+	console.log(notes);
+	console.log("\n");
+	var i = 1; 
+	notes.forEach(function(note){
+		note.id = i;
+		i++;
+	});
+  
+	let newNote = req.body;
+	console.log(newNote);
+	console.log("\n");
 
+  	newNote.id = i;
+	notes.push(newNote);
+
+	console.log(notes);
+	console.log("\n");
+	//let strPath = path.resolve(dbDir, "db.json")
+	//strPath = strPath.toString;
+
+	notes = JSON.stringify(notes);
+	console.log("after JSON.stringify: ");
+	console.log(notes);
+	console.log("\n");
+
+    fs.writeFile(path.resolve(dbDir, "db.json"), notes, function(err){
+		if(err) throw err;
+	});
+	res.json(JSON.parse(notes));
+	// save button writes note object values to json storage object, and also renders values to html display
+});
 
 // delete notes
+/*
 
-  
+app.delete("/api/notes/:id", function(req, res) {
+
+});
+*/
+
 } // end export
